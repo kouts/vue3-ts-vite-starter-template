d37b0a04b726b1b07d5cc8110221c63e7a7e496b
kouts <koutsaftakis@gmail.com>
2023-02-01 21:50:25 +0200

    fix: fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\js$': 'babel-jest',
-    // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig/#examples
-    '^.+\\.tsx?$': [
+    '^.+\\.ts$': [
       'ts-jest',
       {
         tsconfig: 'tsconfig.jest.json'
@@ -15,11 +14,13 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   moduleFileExtensions: ['vue', 'js', 'ts'],
   moduleNameMapper: {
-    // Fix in order for vue-test-utils to work with Jest 28
-    // https://github.com/vuejs/vue-test-utils/issues/1975
-    '^@vue/test-utils$': require.resolve('@vue/test-utils'),
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-  coverageReporters: ['text', 'json-summary']
+  coverageReporters: ['text', 'json-summary'],
+  // Fix in order for vue-test-utils to work with Jest 29
+  // https://test-utils.vuejs.org/migration/#test-runners-upgrade-notes
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  }
 }
