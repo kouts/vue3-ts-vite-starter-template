fc8d6641f52a2f926e66ae1aa0a00a60e7deeb02
kouts <koutsaftakis@gmail.com>
2024-08-03 16:28:20 +0300

    fix: fixed vue env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,8 @@
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<object, object, any>
   export default component
 }
--- eslint.config.js
+++ eslint.config.js
@@ -28,13 +28,14 @@ export default [
       import: pluginImport
     },
     rules: {
+      'import/export': 'error',
       'import/first': 'error',
       'import/no-duplicates': 'error',
       'import/no-mutable-exports': 'error',
       'import/no-named-default': 'error',
       'import/no-self-import': 'error',
       'import/no-webpack-loader-syntax': 'error',
-      'import/order': 'error',
+      'import/no-absolute-path': ['error', { esmodule: true, commonjs: true, amd: false }],
       'import/newline-after-import': ['error', { count: 1 }]
     }
   },
