0749e613b3969a60e89a16b0f44566184fdffd39
kouts <koutsaftakis@gmail.com>
2024-08-17 21:23:57 +0300

    refactor: refactored postcss config to esm

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,15 @@
+import postcssPurgecss from '@fullhuman/postcss-purgecss'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+
 const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
-module.exports = {
+export default {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    tailwindcss,
+    autoprefixer,
     IN_PRODUCTION &&
-      require('@fullhuman/postcss-purgecss')({
+      postcssPurgecss({
         content: ['./**/*.html', './src/**/*.vue'],
         defaultExtractor(content) {
           const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
@@ -17,8 +21,8 @@ module.exports = {
           /^(?!(|.*?:)cursor-move).+-move$/,
           /^router-link(|-exact)-active$/,
           /data-v-.*/,
-          /^active/
-        ]
-      })
-  ]
+          /^active/,
+        ],
+      }),
+  ],
 }
